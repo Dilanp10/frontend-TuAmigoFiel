be0fa497f9fe21f74d2451fd5b6cb79e0bbffda0
DILAN <cabreradilan35@gmail.com>
2025-10-05 22:39:59 -0300

    tres commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-tuamigofiel.onrender.com/api', 
+  baseURL: 'https://backend-tuamigofiel.onrender.com', 
   timeout: 10000,
 });
 
